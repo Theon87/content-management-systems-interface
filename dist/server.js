3b5586c4964f5037e502ed5b502c86a4630bc71c
theon87 <theonfrancis17@gmail.com>
2024-10-03 17:06:12 -0400

    adds default route to server

--- dist/server.js
+++ dist/server.js
@@ -1,27 +1,19 @@
 // DEPENDENCIES
 import express from 'express';
-
-
 // INTERFACES
-
 // DATA
 const PORT = process.env.PORT || 3001;
 const app = express();
-
 // APP/PORTS
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
 // MIDDLEWARE
-
 // ROUTES
-
 // Default response for any other request (Not Found)
-app.use("*",(_req, res) => {
+app.use("*", (_req, res) => {
     res.status(404).send("Nothing found here.");
     console.log("Working!");
 });
-
 // START THE SERVER
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
