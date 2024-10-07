b9a235895ecbaeb6558cf3a1b93bc8763de2acdf
theon87 <theonfrancis17@gmail.com>
2024-10-07 10:04:51 -0400

    modified js files to ts files in src

--- dist/connections.js
+++ dist/connections.js
@@ -1,6 +1,5 @@
 import dotenv from 'dotenv';
 dotenv.config();
-// import { QueryResult } from 'pg';
 import pg from 'pg';
 const { Pool } = pg;
 const pool = new Pool({
@@ -20,13 +19,4 @@ const connectToDb = async () => {
         process.exit(1);
     }
 };
-// Query database
-// const viewDepartments = () => {
-//   pool.query("SELECT * FROM department;", (err: Error, result: QueryResult) => {
-//       if(err){
-//           console.log(err);
-//       }
-//       console.log(result.rows);
-//   })  
-// };
 export { pool, connectToDb };
--- dist/index.js
+++ dist/index.js
@@ -1,74 +1,133 @@
 // DEPENDENCIES
 import inquirer from "inquirer";
-import { pool, connectToDb } from './connections.js';
-await connectToDb();
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // DATA
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // FUNCTIONS
-const viewDepartments = () => {
-    pool.query(`SELECT * FROM department`, (err, result) => {
-        if (err) {
-            console.log(err);
-        }
-        else if (result.rows.length === 0) {
-            console.log("No departments found");
-        }
-        else {
-            console.log(result.rows);
-        }
-    });
-};
-viewDepartments();
+import { viewDepartments, viewRoles, viewEmployees, addDepartment, addRole, addEmployee, updateEmployeeRole, updateEmployeeManager, deleteDepartment, deleteRole, deleteEmployee, viewEmployeesByManager, viewEmployeesByDepartment, viewCombinedSalaryByDepartment } from './pool_queries.js';
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // USER INTERACTIONS
 inquirer
     .prompt([
     {
         // WHEN I start the application
         // THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+        // This works!
         type: 'list',
         message: 'What would you like to do?',
         name: 'purpose',
-        choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role"]
+        choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role", "Update employee manager", "View employees by manager", "View employees by department", "Delete department", "Delete role", "Delete employee", "View combined salary of employees by department", "Quit"]
     }
 ])
     .then((response) => {
     // WHEN I choose to view all departments
     // THEN I am presented with a formatted table showing department names and department ids
+    // This works!
     if (response.purpose === "View all departments") {
-        console.log("a table of department names here");
-        // viewDepartments();
+        viewDepartments();
     }
     else if (response.purpose === "View all roles") {
         // WHEN I choose to view all roles
         // THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
-        console.log("job title, role id, the department that role belongs to, and the salary for that role.");
+        // This works!
+        viewRoles();
     }
     else if (response.purpose === "View all employees") {
         // WHEN I choose to view all employees
         // THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
-        console.log("a formatted table showing employee data");
+        // This works!
+        viewEmployees();
     }
     else if (response.purpose === "Add a department") {
         // WHEN I choose to add a department
         // THEN I am prompted to enter the name of the department and that department is added to the database
-        console.log("prompt to enter the name of the department and that department is added to the database");
+        // This works!
+        addDepartment();
     }
     else if (response.purpose === "Add a role") {
         // WHEN I choose to add a role
         // THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
-        console.log("prompt to enter the name, salary, and department for the role and that role is added to the database");
+        // This works!
+        addRole();
     }
     else if (response.purpose === "Add an employee") {
         // WHEN I choose to add an employee
         // THEN I am prompted to enter the employee's first name, last name, role, and manager, and that employee is added to the database
-        console.log("prompt to enter the employee's first name, last name, role, and manager, and that employee is added to the database");
+        // This works!      
+        addEmployee();
     }
     else if (response.purpose === "Update an employee role") {
         // WHEN I choose to update an employee role
         // THEN I am prompted to select an employee to update and their new role and this information is updated in the database
-        console.log("prompt to select an employee to update and their new role and this information is updated in the database");
+        // This works!
+        updateEmployeeRole();
+    }
+    else if (response.purpose === "Update employee manager") {
+        // BONUS
+        // WHEN I choose to update an employee manager
+        // THEN I am prompted to select an employee to update and their new manager and this information is updated in the database
+        // This works!
+        updateEmployeeManager();
+    }
+    else if (response.purpose === "View employees by manager") {
+        // BONUS
+        // WHEN I choose to view employees by manager
+        // THEN I am presented with a formatted table showing employee data, including manager names
+        // This works!
+        viewEmployeesByManager();
+    }
+    else if (response.purpose === "View employees by department") {
+        // BONUS
+        // WHEN I choose to view employees by department
+        // THEN I am presented with a formatted table showing employee data, including department names
+        // This works!      
+        viewEmployeesByDepartment();
+    }
+    else if (response.purpose === "Delete department") {
+        // BONUS
+        // WHEN I choose to delete a department
+        // THEN the department is deleted from the database
+        // This works!
+        if (response.purpose === "Delete department") {
+            deleteDepartment();
+        }
+    }
+    else if (response.purpose === "Delete role") {
+        // BONUS
+        // WHEN I choose to delete a role
+        // THEN the role is deleted from the database
+        // This works!
+        if (response.purpose === "Delete role") {
+            deleteRole();
+        }
+    }
+    else if (response.purpose === "Delete employee") {
+        // BONUS
+        // WHEN I choose to delete an employee
+        // THEN the employee is deleted from the database
+        // This works!
+        if (response.purpose === "Delete employee") {
+            deleteEmployee();
+        }
+    }
+    else if (response.purpose === "View combined salary of employees by department") {
+        // BONUS
+        // WHEN I choose to view the combined salary of employees by department
+        // THEN I am presented with a formatted table showing department names and combined salaries
+        // This works!
+        if (response.purpose === "View combined salary of employees by department") {
+            viewCombinedSalaryByDepartment();
+        }
+    }
+    else if (response.purpose === "Quit") {
+        // WHEN I choose to quit the application
+        // THEN I am presented with the option to exit the application
+        // This works!
+        console.log("You quit the application");
     }
 })
     .catch(err => {
     console.log(err);
 });
 // INITIALIZATIONS
+// run in terminal: enter "node index" to start the app
